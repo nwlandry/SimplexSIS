fixed the average degree issues

runModelInParallel.py
@@ -34,15 +34,15 @@ endBetaCritFraction = 1.5
 numProcesses = numAlphaPts
 
 # generate adjacency matrices and simplex list
-# k = simplexUtilities.generateUniformDegreeSequence(n, minDeg, maxDeg)
+#k = simplexUtilities.generateUniformDegreeSequence(n, minDeg, maxDeg)
 k = simplexUtilities.generatePowerLawDegreeSequence(n, k0, n, r)
 A = simplexUtilities.generateConfigModelAdjacency(k)
 [simplexList, simplexIndices] = simplexUtilities.generateConfigModelSimplexList(k, simplexSize)
 
 # Calculate critical values
-kAvg = A.sum(axis=0).mean(axis=1).item(0,0)
-kSquaredAvg = ((A.sum(axis=0)*A.sum(axis=1)).mean(axis=0).mean(axis=1)/n).item(0,0)
-kCubedAvg = ((A.sum(axis=1)*A.sum(axis=0)*A.sum(axis=1)).mean(axis=0)/n).item(0,0)
+kAvg = simplexUtilities.avgPowerK(A.sum(axis=0), 1)
+kSquaredAvg =  simplexUtilities.avgPowerK(A.sum(axis=0), 2)
+kCubedAvg =  simplexUtilities.avgPowerK(A.sum(axis=0), 3)
 
 print(kAvg)
 print(kSquaredAvg)
@@ -53,17 +53,18 @@ gamma = 2
 betaCrit = kAvg/kSquaredAvg*gamma
 alphaCrit = (kAvg**2)*kCubedAvg/(kSquaredAvg**3)*gamma
 
-print(betaCrit)
-print(alphaCrit)
 
-beta = np.concatenate([np.linspace(startBetaCritFraction*betaCrit, endBetaCritFraction*betaCrit, numBetaPts),
-                      np.linspace(betaCrit*(endBetaCritFraction-(endBetaCritFraction-startBetaCritFraction)/(numBetaPts-1)), startBetaCritFraction*betaCrit, numBetaPts-1)])
-alpha = np.linspace(startAlphaCritFraction*alphaCrit, endAlphaCritFraction*alphaCrit, numAlphaPts)
+print("beta critical is " + str(betaCrit))
+print("alpha critical is " + str(alphaCrit))
 
-start = time.time()
-equilibria = simplexContagion.generateSISEquilibriaParallelized(A, simplexList, simplexIndices, gamma, beta, alpha, x0, timesteps, dt, avgLength, numProcesses)
-end = time.time()
-print('The elapsed time is ' + str(end-start) + 's')
-
-with open('equilibriaData' + datetime.now().strftime("%m%d%Y-%H%M%S"), 'wb') as file:
-    pickle.dump([alpha, beta, equilibria], file)
+# beta = np.concatenate([np.linspace(startBetaCritFraction*betaCrit, endBetaCritFraction*betaCrit, numBetaPts),
+#                       np.linspace(betaCrit*(endBetaCritFraction-(endBetaCritFraction-startBetaCritFraction)/(numBetaPts-1)), startBetaCritFraction*betaCrit, numBetaPts-1)])
+# alpha = np.linspace(startAlphaCritFraction*alphaCrit, endAlphaCritFraction*alphaCrit, numAlphaPts)
+#
+# start = time.time()
+# equilibria = simplexContagion.generateSISEquilibriaParallelized(A, simplexList, simplexIndices, gamma, beta, alpha, x0, timesteps, dt, avgLength, numProcesses)
+# end = time.time()
+# print('The elapsed time is ' + str(end-start) + 's')
+#
+# with open('equilibriaData' + datetime.now().strftime("%m%d%Y-%H%M%S"), 'wb') as file:
+#     pickle.dump([alpha, beta, equilibria], file)

solveEquilibrium.py
@@ -22,12 +22,12 @@ maxK = 100
 avgK = 75
 avgSquaredK = 5833.3
 betaCrit = avgK/avgSquaredK*gamma
-betaList = np.linspace(0, 0.1, 100)
+betaList = np.linspace(0, 0.1, 20)
 print(betaCrit)
 digitsOfPrecision = 5
 
 # equilibriumV = simplexTheory.solveUniformEquilbrium(gamma, betaList, alpha, minK, maxK, digitsOfPrecision)
-maxK = 10000
+maxK = 1000
 r = 4
 
 equilibriumV = simplexTheory.solvePowerLawEquilbrium(gamma, betaList, alpha, minK, maxK, r, digitsOfPrecision)