2923e22b5a8f86616af7a96b3b678e0d53c7cd88
NLandry91 <nicholas.landry.91@gmail.com>
2019-11-09 10:33:14 -0700

    updated code to add uniform simplices as well as

--- runModel.py
+++ runModel.py
@@ -8,14 +8,17 @@ import simplexContagion
 import pickle
 from datetime import datetime
 import time
+import multiprocessing as mp
+
 # graph parameters
 k0 = 50
 r = 4 # power law exponent
-minDeg = 5
-maxDeg = 10
-n = 11
-#nSimplices = 1000
+minDeg = 50
+maxDeg = 100
+n = 10000
 simplexSize = 3
+isIndependentUniform = False
+degreeDistType = "power-law"
 
 # Epidemic parameters
 initialFraction = 0.5
@@ -25,41 +28,57 @@ x0 = np.random.choice([0, 1], size=n, p=[1-initialFraction, initialFraction])
 timesteps = 1000
 dt = 0.1
 avgLength = int(0.4*timesteps)
-numBetaPts = 4
-numAlphaPts = 2
+numBetaPts = 31
+numAlphaPts = 8
+startAlphaCritFraction = 0.5
+endAlphaCritFraction = 4
+startBetaCritFraction = 0.5
+endBetaCritFraction = 1.5
+numProcesses = numAlphaPts
 
-# generate adjacency matrices and simplex list
-k = simplexUtilities.generatePowerLawDegreeSequence(numPoints, k0, n, r)
+
+# generate degree sequence and adjacency matrix
+if degreeDistType == "uniform":
+    k = simplexUtilities.generateUniformDegreeSequence(n, minDeg, maxDeg)
+elif degreeDistType == "power-law":
+    k = simplexUtilities.generatePowerLawDegreeSequence(n, k0, n, r)
+
 A = simplexUtilities.generateConfigModelAdjacency(k)
-[simplexList, simplexIndices] = simplexUtilities.generateConfigModelSimplexList(k, simplexSize)
 
 # Calculate critical values
-kAvg = A.sum(axis=0).mean(axis=1).item(0,0)
-kSquaredAvg = ((A.sum(axis=0)*A.sum(axis=1)).mean(axis=0).mean(axis=1)/n).item(0,0)
-kCubedAvg = ((A.sum(axis=1)*A.sum(axis=0)*A.sum(axis=1)).mean(axis=0)/n).item(0,0)
-
+kAvg = simplexUtilities.avgPowerK(A.sum(axis=0), 1)
+kSquaredAvg =  simplexUtilities.avgPowerK(A.sum(axis=0), 2)
+kCubedAvg =  simplexUtilities.avgPowerK(A.sum(axis=0), 3)
 print(kAvg)
 print(kSquaredAvg)
 print(kCubedAvg)
 
-# epidemic parameters
-gamma = 2
-betaCrit = kAvg/kSquaredAvg*gamma
-alphaCrit = (kAvg**2)*kCubedAvg/(kSquaredAvg**3)*gamma
+#Generate simplex list
+if isIndependentUniform:
+    [simplexList, simplexIndices] = simplexUtilities.generateUniformSimplexList(n, int(kAvg)*n, simplexSize)
+    # epidemic parameters
+    gamma = 2
+    betaCrit = kAvg/kSquaredAvg*gamma
+    alphaCrit = kCubedAvg/(kAvg**4)*gamma
+else:
+    [simplexList, simplexIndices] = simplexUtilities.generateConfigModelSimplexList(k, simplexSize)
+    # epidemic parameters
+    gamma = 2
+    betaCrit = kAvg/kSquaredAvg*gamma
+    alphaCrit = (kAvg**2)*kCubedAvg/(kSquaredAvg**3)*gamma
 
-print(betaCrit)
-print(alphaCrit)
 
-beta = np.concatenate([np.linspace(0, 2*betaCrit, numBetaPts),
-                      np.linspace(2*betaCrit*(1-1.0/(numBetaPts-1)), 0, numBetaPts-1)])
-alpha = np.linspace(0, 2*alphaCrit, numAlphaPts)
+print("beta critical is " + str(betaCrit))
+print("alpha critical is " + str(alphaCrit))
+
+beta = np.concatenate([np.linspace(startBetaCritFraction*betaCrit, endBetaCritFraction*betaCrit, numBetaPts),
+                      np.linspace(betaCrit*(endBetaCritFraction-(endBetaCritFraction-startBetaCritFraction)/(numBetaPts-1)), startBetaCritFraction*betaCrit, numBetaPts-1)])
+alpha = np.linspace(startAlphaCritFraction*alphaCrit, endAlphaCritFraction*alphaCrit, numAlphaPts)
 
 start = time.time()
-equilibria = simplexContagion.generateSISEquilibria(A, simplexList, simplexIndices, gamma, beta, alpha, x0, timesteps, dt, avgLength)
+equilibria = simplexContagion.generateSISEquilibria(A, simplexList, simplexIndices, gamma, beta, alpha, x0, timesteps, dt, avgLength, numProcesses)
 end = time.time()
 print('The elapsed time is ' + str(end-start) + 's')
 
 with open('equilibriaData' + datetime.now().strftime("%m%d%Y-%H%M%S"), 'wb') as file:
-    pickle.dump(equilibria, file)
-    pickle.dump(alpha, file)
-    pickle.dump(beta, file)
+    pickle.dump([alpha, beta, equilibria], file)
--- runModelInParallel.py
+++ runModelInParallel.py
@@ -9,6 +9,7 @@ import pickle
 from datetime import datetime
 import time
 import multiprocessing as mp
+
 # graph parameters
 k0 = 50
 r = 4 # power law exponent
@@ -16,6 +17,8 @@ minDeg = 50
 maxDeg = 100
 n = 10000
 simplexSize = 3
+isIndependentUniform = False
+degreeDistType = "power-law"
 
 # Epidemic parameters
 initialFraction = 0.5
@@ -33,38 +36,51 @@ startBetaCritFraction = 0.5
 endBetaCritFraction = 1.5
 numProcesses = numAlphaPts
 
-# generate adjacency matrices and simplex list
-#k = simplexUtilities.generateUniformDegreeSequence(n, minDeg, maxDeg)
-k = simplexUtilities.generatePowerLawDegreeSequence(n, k0, n, r)
+
+# generate degree sequence and adjacency matrix
+if degreeDistType == "uniform":
+    k = simplexUtilities.generateUniformDegreeSequence(n, minDeg, maxDeg)
+elif degreeDistType == "power-law":
+    k = simplexUtilities.generatePowerLawDegreeSequence(n, k0, n, r)
+
 A = simplexUtilities.generateConfigModelAdjacency(k)
-[simplexList, simplexIndices] = simplexUtilities.generateConfigModelSimplexList(k, simplexSize)
 
 # Calculate critical values
 kAvg = simplexUtilities.avgPowerK(A.sum(axis=0), 1)
 kSquaredAvg =  simplexUtilities.avgPowerK(A.sum(axis=0), 2)
 kCubedAvg =  simplexUtilities.avgPowerK(A.sum(axis=0), 3)
-
 print(kAvg)
 print(kSquaredAvg)
 print(kCubedAvg)
 
-# epidemic parameters
-gamma = 2
-betaCrit = kAvg/kSquaredAvg*gamma
-alphaCrit = (kAvg**2)*kCubedAvg/(kSquaredAvg**3)*gamma
+#Generate simplex list
+if isIndependentUniform:
+    [simplexList, simplexIndices] = simplexUtilities.generateUniformSimplexList(n, int(kAvg)*n, simplexSize)
+    # epidemic parameters
+    gamma = 2
+    betaCrit = kAvg/kSquaredAvg*gamma
+    alphaCrit = kCubedAvg/(kAvg**4)*gamma
+
+else:
+    [simplexList, simplexIndices] = simplexUtilities.generateConfigModelSimplexList(k, simplexSize)
+    # epidemic parameters
+    gamma = 2
+    betaCrit = kAvg/kSquaredAvg*gamma
+    alphaCrit = (kAvg**2)*kCubedAvg/(kSquaredAvg**3)*gamma
+
 
 
 print("beta critical is " + str(betaCrit))
 print("alpha critical is " + str(alphaCrit))
 
-# beta = np.concatenate([np.linspace(startBetaCritFraction*betaCrit, endBetaCritFraction*betaCrit, numBetaPts),
-#                       np.linspace(betaCrit*(endBetaCritFraction-(endBetaCritFraction-startBetaCritFraction)/(numBetaPts-1)), startBetaCritFraction*betaCrit, numBetaPts-1)])
-# alpha = np.linspace(startAlphaCritFraction*alphaCrit, endAlphaCritFraction*alphaCrit, numAlphaPts)
-#
-# start = time.time()
-# equilibria = simplexContagion.generateSISEquilibriaParallelized(A, simplexList, simplexIndices, gamma, beta, alpha, x0, timesteps, dt, avgLength, numProcesses)
-# end = time.time()
-# print('The elapsed time is ' + str(end-start) + 's')
-#
-# with open('equilibriaData' + datetime.now().strftime("%m%d%Y-%H%M%S"), 'wb') as file:
-#     pickle.dump([alpha, beta, equilibria], file)
+beta = np.concatenate([np.linspace(startBetaCritFraction*betaCrit, endBetaCritFraction*betaCrit, numBetaPts),
+                      np.linspace(betaCrit*(endBetaCritFraction-(endBetaCritFraction-startBetaCritFraction)/(numBetaPts-1)), startBetaCritFraction*betaCrit, numBetaPts-1)])
+alpha = np.linspace(startAlphaCritFraction*alphaCrit, endAlphaCritFraction*alphaCrit, numAlphaPts)
+
+start = time.time()
+equilibria = simplexContagion.generateSISEquilibriaParallelized(A, simplexList, simplexIndices, gamma, beta, alpha, x0, timesteps, dt, avgLength, numProcesses)
+end = time.time()
+print('The elapsed time is ' + str(end-start) + 's')
+
+with open('equilibriaData' + datetime.now().strftime("%m%d%Y-%H%M%S"), 'wb') as file:
+    pickle.dump([alpha, beta, equilibria], file)
